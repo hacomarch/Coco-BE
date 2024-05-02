122d01b5dc006724190a821042487c724d9d283a
saokiritoni <leesoeun2746@dgu.ac.kr>
2024-05-02 16:58:36 +0900

    member

---
+++ src/main/java/coco/ide/global/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package coco.ide.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()  // 개발중) 정적 리소스에 대한 접근 허용
+                        .requestMatchers("/api/docs/**").permitAll()                  // 개발중) API 문서 경로 접근 허용
+                        //.requestMatchers("/h2-console/**").permitAll()                // 개발중) H2 데이터베이스 콘솔 접근 허용
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)         // 개발중) csrf 보호 비활성화
+                .headers(headers -> headers
+                        .addHeaderWriter(new StaticHeadersWriter("X-Frame-Options", "DENY"))); // 개발중) frame 옵션 비활성화
+
+        return http.build();
+    }
+
+}

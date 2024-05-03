3f930d183cc11d47afe7a1383b96f4418b67f7f2
popeye0618 <popeye0618@gmail.com>
2024-05-03 10:06:25 +0900

    프로젝트 이름 변경까지 구현

--- src/main/java/coco/ide/ideapp/projects/requestdto/UpdateProjectNameForm.java
+++ src/main/java/coco/ide/ideapp/projects/requestdto/UpdateProjectNameForm.java
@@ -1,13 +1,8 @@
 package coco.ide.ideapp.projects.requestdto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
-@AllArgsConstructor
 public class UpdateProjectNameForm {
-    private final String newName = null;
+    private String newName = null;
 }

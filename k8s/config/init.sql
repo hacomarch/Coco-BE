52f7cbc79dc1dbdded00f28a792c36d659d5ff55
eunbipnoeul <eunbipnoeul@nate.com>
2024-05-14 15:54:24 +0900

    ㄴㄴ

--- k8s/config/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS `cocoide` DEFAULT CHARACTER SET utf8mb4;
-
-GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
-GRANT ALL ON cocoide.* TO 'root'@'localhost';
-FLUSH PRIVILEGES;
-
-USE `cocoide`;

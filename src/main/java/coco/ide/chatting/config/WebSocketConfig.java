7c51c645437673b49396fedf62af6aaf49b38225
popeye0618 <popeye0618@gmail.com>
2024-05-16 19:35:10 +0900

    .

--- src/main/java/coco/ide/chatting/config/WebSocketConfig.java
+++ src/main/java/coco/ide/chatting/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer, WebSoc
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
         registry.addEndpoint("/ws")         // stomp 통신
-                .setAllowedOrigins("localhost:3000")
+                .setAllowedOrigins("3000")
                 .withSockJS();
     }
     @Override

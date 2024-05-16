047561e77582229a76408edb063a2ce0d2254a83
popeye0618 <popeye0618@gmail.com>
2024-05-16 19:09:36 +0900

    .

--- src/main/java/coco/ide/webconfig/WebConfig.java
+++ src/main/java/coco/ide/webconfig/WebConfig.java
@@ -5,14 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//public class WebConfig {
-//
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
 //                registry.addMapping("/**")
 ////                        .allowedOrigins("http://localhost:8080",
 ////                                "http://localhost:3000",
@@ -21,7 +21,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 ////                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 ////                        .allowedHeaders("*")
 //                        .allowedOrigins()
-//            }
-//        };
-//    }
-//}
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*") // 안에 해당 주소를 넣어도 됨
+                        .allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
+                        .exposedHeaders("Authorization", "RefreshToken");
+                //.allowCredentials(true);
+            }
+        };
+    }
+}

82dddeec5639a35bc7d5b6ea11f1d9c40840aa5d
popeye0618 <popeye0618@gmail.com>
2024-05-16 16:09:28 +0900

    ez

--- src/main/java/coco/ide/ideapp/projects/ProjectService.java
+++ src/main/java/coco/ide/ideapp/projects/ProjectService.java
@@ -40,7 +40,7 @@ public class ProjectService {
         Project savedProject = projectRepository.save(project);
 
         //프로젝트 생성 시 filedb 밑에 폴더 생성
-        String dirPath = "/app/filedb/" + savedProject.getMember().getMemberId() + "/" + savedProject.getProjectId();
+        String dirPath = "filedb/" + savedProject.getMember().getMemberId() + "/" + savedProject.getProjectId();
         File directory = new File(dirPath);
         if (!directory.exists()) {
             boolean created = directory.mkdirs();

12f9aaac044017c555a6a0a81a854252e8e49822
hacomarch <haco_march@naver.com>
2024-05-29 22:57:30 +0900

    [리팩토링] 커스텀 예외 생성 ProjectNotFound, UserNotFound

---
+++ src/main/java/coco/ide/ideapp/exception/ProjectNotFoundException.java
@@ -0,0 +1,7 @@
+package coco.ide.ideapp.exception;
+
+public class ProjectNotFoundException extends RuntimeException{
+    public ProjectNotFoundException() {
+        super("프로젝트를 찾을 수 없습니다.");
+    }
+}
---
+++ src/main/java/coco/ide/ideapp/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package coco.ide.ideapp.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException() {
+        super("유저를 찾을 수 없습니다.");
+    }
+}

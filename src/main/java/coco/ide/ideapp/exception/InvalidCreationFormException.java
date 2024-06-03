19164fdceecca251ff7ac325c3cf6aee81338091
hacomarch <haco_march@naver.com>
2024-06-03 22:42:19 +0900

    [리팩토링] 예외 처리 메시지 수정

--- src/main/java/coco/ide/ideapp/exception/InvalidCreationFormException.java
+++ src/main/java/coco/ide/ideapp/exception/InvalidCreationFormException.java
@@ -2,6 +2,6 @@ package coco.ide.ideapp.exception;
 
 public class InvalidCreationFormException extends RuntimeException {
     public InvalidCreationFormException() {
-        super("잘못된 생성 양식입니다.");
+        super("잘못된 양식입니다.");
     }
 }
